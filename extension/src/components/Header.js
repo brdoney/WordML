9c4d1d43927973d068b9b5fe7cb37a5bed43b61d
blakemarterella <blake@marterella.com>
2022-02-26 17:56:26 -0500

    change the order and add info

--- extension/src/components/Header.js
+++ extension/src/components/Header.js
@@ -1,10 +1,13 @@
 import '../assets/css/Header.css';
+import '../assets/js/scraper.js';
 
 function header() {
   return (
     <div class="header">
         <h1>Wordle ML</h1>
         <p1>A wordle solution assistant!</p1>
+        <br />
+        <p1><b>First Word: </b></p1 ><p1 id="word01"></p1>
     </div>
   );
 }
