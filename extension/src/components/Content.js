1f888e092c1d2667bbfc1cf9085d9a90319cd472
blakemarterella <blake@marterella.com>
2022-02-26 17:31:35 -0500

    Redo the file structure

---
+++ extension/src/components/Content.js
@@ -0,0 +1,11 @@
+import '../assets/css/Content.css';
+
+function content() {
+  return (
+    <div class="content">
+      <p1>I am the content</p1>
+    </div>
+  );
+}
+
+export default content;
---
+++ extension/src/components/Footer.js
@@ -0,0 +1,11 @@
+import '../assets/css/Footer.css';
+
+function footer() {
+  return (
+    <div class="footer">
+      <p1>I am the footer</p1>
+    </div>
+  );
+}
+
+export default footer;
