70e11f762cbb1a677695d03cef1776a88652442d
blakemarterella <blake@marterella.com>
2022-02-26 22:21:51 -0500

    push content

--- extension/public/content.js
+++ extension/public/content.js
@@ -5,13 +5,9 @@ chrome.runtime.sendMessage({greeting: document.all[0].outerHTML}, function(respo
     console.log(response.farewell);
   });
 
-window.addEventListener("keydown", function(event) {
-    if (event.key === "Enter") {
-        console.log("Enter");
-    }
-});
-
 function parseSource() {
-    var temp = document.getElementsByTagName('game-app').item(0);
-    var startPos = 
+    var el = document.createElement( 'html' );
+    el = document.getElementsByTagName('game-app').item(0);
+    var str = String(el.outerHTML);
+    
 }
