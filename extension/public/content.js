306e4e8cc0980a93fa39220079a3b14e14246e80
Brendan Doney <bre.doney@gmail.com>
2022-02-27 02:06:38 -0500

    Correctly format JSON

--- extension/public/content.js
+++ extension/public/content.js
@@ -1,79 +1,69 @@
 /*global chrome*/
 var correctWords = 0;
 
-chrome.runtime.sendMessage({greeting: document.all[0].outerHTML}, function(response) {
+chrome.runtime.sendMessage(
+  { greeting: document.all[0].outerHTML },
+  function (response) {
     console.log(response.farewell);
-  });
+  }
+);
 
 async function parseSource() {
-    var foo = document.createElement( 'html' );
-    foo = document.getElementsByTagName('game-app').item(0).shadowRoot.getElementById('board');
+  var foo = document.createElement("html");
+  foo = document
+    .getElementsByTagName("game-app")
+    .item(0)
+    .shadowRoot.getElementById("board");
 
-    var tempCorrectWords = 0;
-    for (let i = 0; i < correctWords + 1; i++) {
-      var row = foo.children[i];
-      var countValid = 0;
-      for (const letter of row.shadowRoot.children[1].children) {
-        if (letter.getAttribute('evaluation') != null) {
-          countValid ++;
-        }
-      }
-      // If the word has 5 correct values, add it to the words
-      if (countValid === 5) {
-        tempCorrectWords++;
+  var tempCorrectWords = 0;
+  for (let i = 0; i < correctWords + 1; i++) {
+    var row = foo.children[i];
+    var countValid = 0;
+    for (const letter of row.shadowRoot.children[1].children) {
+      if (letter.getAttribute("evaluation") != null) {
+        countValid++;
       }
     }
-    if (tempCorrectWords > correctWords) {
-        correctWords++;
-        console.log("fuck");
-        console.log(getWordData());
+    // If the word has 5 correct values, add it to the words
+    if (countValid === 5) {
+      tempCorrectWords++;
     }
+  }
+  if (tempCorrectWords > correctWords) {
+    correctWords++;
+    const data = getWordData();
+    console.log(data);
+    chrome.runtime.sendMessage({ message: data });
+  }
 }
 
 function getWordData() {
-  var row = document.createElement( 'html' );
-  row = document.getElementsByTagName('game-app').item(0).shadowRoot.getElementById('board').children[correctWords - 1];
+  let row = document.createElement("html");
+  row = document
+    .getElementsByTagName("game-app")
+    .item(0)
+    .shadowRoot.getElementById("board").children[correctWords - 1];
   var position = 0;
-  let jsonAbsent = '"absent": "';
-  let jsonPresent = '"present": [';
-  let jsonCorrect = '"correct": [';
-  let jsonReturn = '{';
+	const entry = { present: [], correct: {}, absent: "" };
   for (const letter of row.shadowRoot.children[1].children) {
-      var tempEval = letter.getAttribute('evaluation');
-      if (tempEval === "absent") {
-        jsonAbsent += letter.getAttribute('letter').toLowerCase();
-      }
-      else if(tempEval === "present") {
-        let tempJSON = '{';
-        tempJSON += '"letter": "' + letter.getAttribute('letter').toLowerCase() + '",';
-        tempJSON += '"position": ' + position + ',';
-        tempJSON += '}'
-        jsonPresent += tempJSON;
-        if (position !== 5) {
-          jsonPresent += ',';
-        }
-      }
-      else if(tempEval === "correct") {
-        let tempJSON = '{';
-        tempJSON += '"letter": "' + letter.getAttribute('letter').toLowerCase() + '",';
-        tempJSON += '"position": ' + position + ',';
-        tempJSON += '}'
-        jsonCorrect += tempJSON;
-        if (position !== 5) {
-          jsonCorrect += ',';
-        }
-      }
-      position ++;
+    const tempEval = letter.getAttribute("evaluation");
+    if (tempEval === "absent") {
+      entry.absent += letter.getAttribute("letter").toLowerCase();
+    } else if (tempEval === "present") {
+      entry.present.push({
+        letter: letter.getAttribute("letter").toLowerCase(),
+        position: position,
+      });
+    } else if (tempEval === "correct") {
+      entry.correct[position] = letter.getAttribute("letter").toLowerCase();
+    }
+    position++;
   }
-  jsonAbsent += '",';
-  jsonPresent += "],"
-  jsonCorrect += "]";
-  jsonReturn += jsonAbsent + jsonPresent + jsonCorrect + '}';
-  return jsonReturn;
+  return entry;
 }
 
 document.onkeypress = function (e) {
-  if (e.code === 'Enter') {
+  if (e.code === "Enter") {
     parseSource();
   }
 };
