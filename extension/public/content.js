683f0b0499f23f8b7dbfb6f9805955270f2acee2
blakemarterella <blake@marterella.com>
2022-02-26 22:10:44 -0500

    set up the background and content

---
+++ extension/public/content.js
@@ -0,0 +1,16 @@
+/*global chrome*/
+console.log("Hello World - Contenet");
+
+chrome.runtime.sendMessage({greeting: document.all[0].outerHTML}, function(response) {
+    console.log(response.farewell);
+  });
+
+window.addEventListener("keydown", function(event) {
+    if (event.key === "Enter") {
+        console.log("Enter");
+    }
+});
+
+function parseSource() {
+    var temp = document.getElementsByTagName('game-app').item(0);
+}
