8d84781f2fd0d2c2f638fa245a850df4e0d56896
Brendan Doney <bre.doney@gmail.com>
2022-02-27 01:46:12 -0500

    Render table based on data

--- extension/src/components/MainContent.js
+++ extension/src/components/MainContent.js
@@ -1,59 +1,76 @@
-import '../assets/css/MainContent.css';
+/*global chrome*/
+import "../assets/css/MainContent.css";
+import { useEffect, useState } from "react";
 
-function content() {
+function Row(props) {
+	const scorePercent = `${(props.score * 100).toFixed(2)}`;
+
+  return (
+    <tr>
+      <td>{props.word}</td>
+      <td>
+        <progress value={props.score}></progress>
+      </td>
+      <td>{scorePercent}%</td>
+    </tr>
+  );
+}
+
+function Content() {
+  const [data, setData] = useState({
+    entries: [
+      {
+        score: 1.0,
+        word: "tares",
+      },
+      {
+        score: 0.9940471601858769,
+        word: "lares",
+      },
+      {
+        score: 0.9867535607370882,
+        word: "rales",
+      },
+      {
+        score: 0.98376684188455,
+        word: "rates",
+      },
+      {
+        score: 0.9804624629607341,
+        word: "nares",
+      },
+    ],
+  });
+
+  const processChange = (message) => {
+    console.log(message.greeting);
+    setData(message.data);
+  };
+
+  useEffect(() => {
+    chrome.runtime.onMessage.addListener(processChange);
+    return () => chrome.runtime.onMessage.removeListener(processChange);
+  }, []);
+
+  const rows = [];
+  for (const row of data["entries"]) {
+    rows.push(<Row word={row["word"]} score={row["score"]} />);
+  }
+
   return (
     <div class="content">
       <table id="wordTable">
-          <thead>
+        <thead>
           <tr>
             <th>Word</th>
             <th>Rating</th>
             <th>Score</th>
           </tr>
-          </thead>
-          <tr>
-            <td id="word1">Word1</td>
-            <td>
-            <progress id="determinate1" value="80" min="0"max ="100">
-            </progress>
-            </td>
-            <td id="score1">Score1</td>
-          </tr>
-          <tr>
-            <td id="word2">Word2</td>
-            <td>
-            <progress id="determinate2" value="65" min="0"max ="100">
-            </progress>
-            </td>
-            <td id="score2">Score2</td>
-          </tr>
-          <tr>
-            <td id="word3">Word3</td>
-            <td>
-            <progress id="determinate3" value="60" min="0"max ="100">
-            </progress>
-            </td>
-            <td id="score3">Score3</td>
-          </tr>
-          <tr>
-            <td id="word4">Word4</td>
-            <td>
-            <progress id="determinate4" value="50" min="0"max ="100">
-            </progress>
-            </td>
-            <td id="score4">Score4</td>
-          </tr>
-          <tr>
-            <td id="word5">Word5</td>
-            <td>
-            <progress id="determinate5" value="45" min="0"max ="100">
-            </progress>
-            </td>
-            <td id="score5">Score5</td>
-          </tr>
-        </table>
+        </thead>
+        {rows}
+      </table>
     </div>
   );
 }
 
-export default content;
+export default Content;
