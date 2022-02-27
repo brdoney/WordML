bc0a378ba8d01dcb27c8564bf2576816464ed04b
blakemarterella <blake@marterella.com>
2022-02-26 19:18:12 -0500

    get the url of the current page

--- extension/src/background.js
+++ extension/src/background.js
@@ -1,15 +1,22 @@
 /*global chrome*/
 
+// When wordle is opened, run this
 window.onload = function() {
-  console.log("onload" + Date())
+    scrapeRows();
 }
 
-document.addEventListener('keyup', (e) => {
-    if (e.code === "l") {
-        scrapeWord();
-    }
-});
-
-function scrapeWord() {
-    console.log("Enter key was hit, checking the board...");
+function scrapeRows() {
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+        // since only one tab should be active and in the current window at once
+        // the return variable should only have one entry
+        var xhr = new XMLHttpRequest();
+        var tabURL = tabs[0].url;
+        xhr.open("GET", tabURL, true);
+        xhr.onreadystatechange = function() {
+        if (xhr.readyState == 4) {
+            alert(xhr.responseText);
+            }
+        }
+        xhr.send();
+     });
 }
