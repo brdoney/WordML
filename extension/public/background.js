dd591eabafbd42f32dc5c5a415515f3153236aa8
blakemarterella <blake@marterella.com>
2022-03-09 16:10:46 -0500

    Extension will only open on wordle

--- extension/public/background.js
+++ extension/public/background.js
@@ -11,3 +11,20 @@ chrome.runtime.onMessage.addListener((message, sender, sendReponse) => {
     boardState = message.message;
   }
 });
+
+chrome.declarativeContent.onPageChanged.removeRules(undefined, 
+  function() {
+     // With a new rule ...
+     chrome.declarativeContent.onPageChanged.addRules([
+     {
+      // That fires when a page's URL contains a 'g' ...
+      conditions: [
+      new chrome.declarativeContent.PageStateMatcher({
+        pageUrl: { urlContains: 'nytimes.com/games/wordle' },
+      })
+     ],
+      // And shows the extension's page action.
+      actions: [ new chrome.declarativeContent.ShowPageAction() ]
+    }
+  ]);
+});
